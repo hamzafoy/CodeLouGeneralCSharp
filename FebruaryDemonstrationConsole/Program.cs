5d1b429af21230ec0be2bf84f71358d0ea155573
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-12 13:46:11 -0500

    Made changes to layout and file names.

---
+++ FebruaryDemonstrationConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CodeLouisvilleLibrary;
+
+namespace FebruaryDemonstration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Anta Tafhamu? ");
+            if (doesThisMakeSense)
+                Console.WriteLine("I am successful");
+            else
+                Console.WriteLine("I am unsuccessful");
+        }
+    }
+}
