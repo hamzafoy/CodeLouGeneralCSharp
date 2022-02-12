cedade40d871c72e14d0d8dcf196c6935db0f5c8
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-12 14:12:46 -0500

    Tweaked a few console-write statements.

--- FebruaryDemonstrationConsole/Program.cs
+++ FebruaryDemonstrationConsole/Program.cs
@@ -7,11 +7,11 @@ namespace FebruaryDemonstration
     {
         static void Main(string[] args)
         {
-            bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Anta Tafhamu? ");
+            bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Anta Tafhamu? ");//Anta Tafhamu is 'Arabic' for 'Do you understand?'
             if (doesThisMakeSense)
-                Console.WriteLine("I am successful");
+                Console.WriteLine("I have successfully understood");
             else
-                Console.WriteLine("I am unsuccessful");
+                Console.WriteLine("I have not successfully understood the assignment!");
         }
     }
 }
