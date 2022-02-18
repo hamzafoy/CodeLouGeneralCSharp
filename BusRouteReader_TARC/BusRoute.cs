aa4fcddc2bcd1f856738cba1b932d79c282c6146
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-18 16:29:34 -0500

    Initialized repo to go along with the Array/Collections series on Pluralsight

---
+++ BusRouteReader_TARC/BusRoute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TARC_BusRouteReader_BusRouteClass
+{
+    public class BusRoute
+    {
+        public int Number { get; }
+        public string Origin { get; }
+        public string Destination { get; }
+        public BusRoute(int number, string origin, string destination)
+        {
+            this.Destination = destination;
+            this.Number = number;
+            this.Origin = origin;
+        }
+        public override string ToString() => $"{Number}: {Origin} -> {Destination}";
+    }
+}
---
+++ BusRouteReader_TARC/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TARC_BusRouteReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
