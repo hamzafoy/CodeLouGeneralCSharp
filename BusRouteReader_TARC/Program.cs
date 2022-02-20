0a296fd83e1b220336c0ac62e97fe1b9d4910532
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-19 22:36:19 -0500

    Inserted basic syntax without Collections use to Program.cs

--- BusRouteReader_TARC/Program.cs
+++ BusRouteReader_TARC/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TARC_BusRouteReader_BusRouteClass;
 
 namespace TARC_BusRouteReader
 {
@@ -7,7 +8,11 @@ namespace TARC_BusRouteReader
     {
         static void Main(string[] args)
         {
-            
+            BusRoute route17 = new BusRoute(17, "8th Street", "Michael Edwards Dr");
+            BusRoute route29 = new BusRoute(29, "Cane Run Rd", "Oxmoor Center");
+
+            Console.WriteLine(route17);
+            Console.WriteLine(route29);
         }
     }
 }
