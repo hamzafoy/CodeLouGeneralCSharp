bb5be859aa30867b39a9f1a41e70cd0a8151ce87
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-21 17:27:20 -0500

    Testing out the ability to initialize Objects from classes inside an array declaration.

--- BusRouteReader_TARC/Program.cs
+++ BusRouteReader_TARC/Program.cs
@@ -10,10 +10,11 @@ namespace TARC_BusRouteReader
         {
             BusRoute route17 = new BusRoute(17, "8th Street", "Michael Edwards Dr");
             BusRoute route29 = new BusRoute(29, "Cane Run Rd", "Oxmoor Center");
+            BusRoute route25 = new BusRoute(25, "Shawnee Park", "Accomack Dr");
 
             BusRoute[] routes =
             {
-                route17, route29
+                route17, route29, route25, new BusRoute(63, "Terry Rd", "4th St")
             };
 
             foreach (BusRoute route in routes)
