4e5c56a3b67fd07143a7aaa514a1f2216ad54d52
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-22 14:45:36 -0500

    Created a new method to generate a List<T> of bus routes.

--- BusRouteReader_TARC/RouteRepository.cs
+++ BusRouteReader_TARC/RouteRepository.cs
@@ -15,7 +15,16 @@ namespace TARC_BusRouteReader_RouteRepository
                 new BusRoute(29, stops: new string[] {"Cane Run Rd", "7th Street Rd", "Taylor Blvd", "Winkler Ave", "Eastern Pkwy", "Bardstown Rd", "Shelbyville Rd", "Oxmoor Center"}),
                 new BusRoute(25, stops: new string[] {"Shawnee Park", "Cecil Ave", "NIA Center", "Dumesnil St", "Oak St", "Grinstead Dr", "Shelbyville Rd", "Westport Rd", "N Hurstbourne Pkwy", "Springhurst Blvd"}),
                 new BusRoute(63, stops: new string[] {"Terry Rd", "Greenwood Rd", "Greenbelt Hwy", "Cane Run Rd", "Crums Ln", "7th Street Rd", "S 7th St", "S 5th St", "N 4th St"})
-            };
+            
+                
+        };
 
+        public static List<BusRoute> TarcRoutesList()
+        {
+            List<BusRoute> busStops = new List<BusRoute>();
+            busStops.Add(new BusRoute(17, stops: new string[] { "N 8th St", "Liberty St", "Grinstead Dr", "Douglass Blvd", "Bashford Manor Ln", "S Hurstbourne Pkwy", "Glenmary Farms" }));
+            busStops.Add(new BusRoute(29, stops: new string[] { "Cane Run Rd", "7th Street Rd", "Taylor Blvd", "Winkler Ave", "Eastern Pkwy", "Bardstown Rd", "Shelbyville Rd", "Oxmoor Center" }));
+            return busStops;
+        }
     }
 }
